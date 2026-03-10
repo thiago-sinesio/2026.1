e8d980cec28f6606b2f461817bfe9446deda47d3
Thiago Sinesio <thiagosinsiodasilva@gmail.com>
2026-03-10 20:01:58 -0300

    primeiro exercicio

---
+++ exercicio-01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, soma;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    soma = num1 + num2;
+
+    cout << "A soma de " << num1 << " + " << num2 << " = " << soma << endl;
+
+    return 0;
+}
