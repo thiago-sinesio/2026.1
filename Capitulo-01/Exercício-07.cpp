5827c1f0c8065ce7b55bc883dd7bcb71e29cb200
Thiago Sinésio <97791389+thiago-sinesio@users.noreply.github.com>
2026-03-16 20:32:56 -0300

    Exercicio 7

---
+++ Capitulo-01/Exercício-07.cpp
@@ -0,0 +1,14 @@
+//7. Elabore um programa que receba um número inteiro do usuário e 
+//exiba o seu valor elevado ao quadrado.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"Digite um numero: ";
+    cin>>num;
+
+    cout<< num << " elevado ao quadrado é: " << num * num << endl;
+    return 0;
+}
