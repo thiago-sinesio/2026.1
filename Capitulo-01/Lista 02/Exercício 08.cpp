4bc2709e4579be8cf74cc39342f01b7788f76756
Thiago Sinésio <97791389+thiago-sinesio@users.noreply.github.com>
2026-03-16 23:43:11 -0300

    Add voting eligibility check program

---
+++ Capitulo-01/Lista 02/Exercício 08.cpp
@@ -0,0 +1,18 @@
+//8. Faça um programa que leia o ano de nascimento de uma pessoa e informe
+//se ela já pode votar (idade mínima 16 anos) ou não.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"Informe seu ano de nascimento: ";
+    cin>>num;
+    
+    if ((2026 - num) >= 16){
+        cout<<"Pode votar.";
+    } else {
+        cout<<"Não pode votar.";
+    }
+    return 0;
+}
